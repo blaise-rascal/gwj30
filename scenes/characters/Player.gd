b70b35b566f86a0b8f60c10bcd57dcbf20f0cfc1
npb <apr0alt@gmail.com>
2021-02-13 18:19:29 -0500

    tweaks to movement

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -2,12 +2,14 @@ extends KinematicBody2D
 
 const MAX_SPEED : int = 260 # Pixels/second.
 const ACCELERATION_MODIFIER : int = 1200 # Pixels/second
-const DRAG_COEF: float = -0.9 # Ratio
+const DRAG_COEF: float = -0.999 # Ratio
 const VELOCITY_TO_STOP_DRAGGING : int = 16 # Pixels/second
-const JUMP_ACCELERATION : int = 20 # Some unit, not really sure what tbh
+const JUMP_ACCELERATION : int = 18 # Some unit, not really sure what tbh
 const GRAVITY : int = 1 # See above comment
 
-onready var onGround = $OnGround
+onready var on_ground = $OnGround
+onready var on_ground_left = $OnGroundLeft
+onready var on_ground_right = $OnGroundRight
 
 var velocity = Vector2()
 
@@ -16,6 +18,9 @@ func _ready():
 	pass # Replace with function body.
 
 
+func is_on_ground():
+	return on_ground.is_colliding() or on_ground_left.is_colliding() or on_ground_right.is_colliding()
+
 
 func _physics_process(delta):
 	var acceleration = Vector2(0,0)  # The player's movement vector.
@@ -23,14 +28,13 @@ func _physics_process(delta):
 		acceleration.x += 1
 	if Input.is_action_pressed("ui_left"):
 		acceleration.x -= 1
-	if Input.is_action_just_pressed("ui_up") && onGround.is_colliding():
+	if Input.is_action_just_pressed("ui_up") && is_on_ground():
 		acceleration.y -= JUMP_ACCELERATION
 
 	acceleration.x = clamp(acceleration.x, -1, 1)
 	
 	# Apply gravity
-	if !onGround.is_colliding():
-		acceleration.y += GRAVITY
+	acceleration.y += GRAVITY
 	
 	#See if the character needs to drag to a halt
 	if acceleration.x == 0:
@@ -50,6 +54,6 @@ func _physics_process(delta):
 	if acceleration.length() > 0:
 		acceleration *= ACCELERATION_MODIFIER
 		velocity += acceleration * delta
-		if(velocity.x > MAX_SPEED):
+		if(abs(velocity.x) > MAX_SPEED):
 			velocity.x = clamp(velocity.x, -1, 1) * MAX_SPEED
 	velocity = move_and_slide(velocity, Vector2(0, 1))
