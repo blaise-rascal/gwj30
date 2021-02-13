0f7019242c60e31ce3af67230b13da10d1d5f914
npb <noahpberman@gmail.com>
2021-02-13 12:17:14 -0500

    implemented jumping and one-way platforms

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -4,6 +4,10 @@ const MAX_SPEED : int = 260 # Pixels/second.
 const ACCELERATION_MODIFIER : int = 1200 # Pixels/second
 const DRAG_COEF: float = -0.9 # Ratio
 const VELOCITY_TO_STOP_DRAGGING : int = 16 # Pixels/second
+const JUMP_ACCELERATION : int = 20 # Some unit, not really sure what tbh
+const GRAVITY : int = 1 # See above comment
+
+onready var onGround = $OnGround
 
 var velocity = Vector2()
 
@@ -19,17 +23,22 @@ func _physics_process(delta):
 		acceleration.x += 1
 	if Input.is_action_pressed("ui_left"):
 		acceleration.x -= 1
+	if Input.is_action_just_pressed("ui_up") && onGround.is_colliding():
+		acceleration.y -= JUMP_ACCELERATION
 
-	acceleration = acceleration.normalized()
+	acceleration.x = clamp(acceleration.x, -1, 1)
+	
+	# Apply gravity
+	if !onGround.is_colliding():
+		acceleration.y += GRAVITY
 	
 	#See if the character needs to drag to a halt
-	if acceleration.length() == 0:
+	if acceleration.x == 0:
 		#$CharAnims.play("idle")
-		if(velocity.length()>VELOCITY_TO_STOP_DRAGGING) :
-			acceleration = DRAG_COEF * velocity.normalized()
+		if(abs(velocity.x) > VELOCITY_TO_STOP_DRAGGING) :
+			acceleration.x = DRAG_COEF * clamp(velocity.x, -1, 1)
 		else:
 			velocity.x = 0
-			velocity.y = 0
 #	else:
 #		if(acceleration.x >0):
 #			$Sprite.flip_h=true
@@ -37,12 +46,10 @@ func _physics_process(delta):
 #			$Sprite.flip_h=false
 #		$CharAnims.play("walk")
 	
-	
 	# Apply acceleration modifier & do other stuff
 	if acceleration.length() > 0:
-		acceleration = acceleration * ACCELERATION_MODIFIER
+		acceleration *= ACCELERATION_MODIFIER
 		velocity += acceleration * delta
-		if(velocity.length() > MAX_SPEED):
-			velocity = velocity.normalized() * MAX_SPEED
-			
-	velocity = move_and_slide(velocity)
+		if(velocity.x > MAX_SPEED):
+			velocity.x = clamp(velocity.x, -1, 1) * MAX_SPEED
+	velocity = move_and_slide(velocity, Vector2(0, 1))
