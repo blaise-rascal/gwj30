86755d4e573ce8bbab1e45b411cb71a232777974
blaise-rascal <65874622+blaise-rascal@users.noreply.github.com>
2021-02-15 00:24:55 -0500

    Streamlined movement code, added variable jump height, decreased coyote time, added rudimentary camera to player, expanded level

--- scenes/characters/NewPlayer.gd
+++ scenes/characters/NewPlayer.gd
@@ -1,13 +1,16 @@
 extends KinematicBody2D
 
 const MAX_SPEED : int = 260 # Pixels/second.
-const ACCELERATION_MODIFIER : int = 1500 # Pixels/second
-const DRAG_COEF : float = -0.999 # Ratio
-const AIR_DRAG : float = -0.3 # Ratio
+const LEFT_RIGHT_ACCELERATION: int = 1700 # Pixels/second2
+const GROUND_DRAG_DECELERATION : int = 700 # Pixels/second2
+const AIR_DRAG_DECELERATION : int = 700 # Pixels/second2
 const VELOCITY_TO_STOP_DRAGGING : int = 16 # Pixels/second
-const JUMP_ACCELERATION : int = 17 # Some unit, not really sure what tbh
-const WALL_JUMP_ACCELERATION : int = 12 # see above comment
-const GRAVITY : int = 1 # See above comment
+const JUMP_VERTICAL_VELOCITY : int = 400 # Pixels/second
+const WALL_JUMP_VERTICAL_VELOCITY : int = 300 # Pixels/second
+const WALL_JUMP_HORIZONTAL_VELOCITY : int = 260 # Pixels/second
+const REGULAR_GRAVITY : int = 2200 #Pixels/second2
+const GRAVITY_WITH_UP_HELD : int = 1100 #Pixels/second2
+#TODO: maybe increase gravity if you hold down? like celeste?
 
 onready var on_ground = $OnGround
 onready var on_ground_left = $OnGroundLeft
@@ -31,36 +34,43 @@ func is_on_ground():
 
 func _physics_process(delta):
 	var acceleration = Vector2(0,0)  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		acceleration.x += 1
+	
+	#ACCELERATION IS CALCULATED EVERY FRAME FROM SCRATCH
+	#VELOCITY IS PRESERVED FROM FRAME TO FRAME, BUT MODIFIED BY ACCELERATION (AND SOMETIMES SET EXPLICITY, LIKE WHEN YOU JUMP)
+	
+	if Input.is_action_pressed("ui_right"): #TODO: Make it so that it only adds walk acceleration if the velocity is not too high already (rather than handling this case later on)
+		acceleration.x = LEFT_RIGHT_ACCELERATION
 	if Input.is_action_pressed("ui_left"):
-		acceleration.x -= 1
+		acceleration.x = -LEFT_RIGHT_ACCELERATION
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_ground():
-			velocity.y = 0
-			acceleration.y -= JUMP_ACCELERATION
+			velocity.y = -JUMP_VERTICAL_VELOCITY
 		elif on_left_wall.is_colliding():
-			velocity.y = 0
-			acceleration.y -= WALL_JUMP_ACCELERATION
-			velocity.x = MAX_SPEED
+			velocity.y = -WALL_JUMP_VERTICAL_VELOCITY
+			velocity.x = WALL_JUMP_HORIZONTAL_VELOCITY
 		elif on_right_wall.is_colliding():
-			velocity.y = 0
-			acceleration.y -= WALL_JUMP_ACCELERATION
-			velocity.x = -MAX_SPEED
+			velocity.y = -WALL_JUMP_VERTICAL_VELOCITY
+			velocity.x = -WALL_JUMP_HORIZONTAL_VELOCITY
 
-	acceleration.x = clamp(acceleration.x, -1, 1)
 	
 	# Apply gravity
-	acceleration.y += GRAVITY
+	if(Input.is_action_pressed("ui_up") && velocity.y<0): #If you're jumping and the up button is held, make gravity lower
+		#print("UP HELD")
+		acceleration.y = GRAVITY_WITH_UP_HELD
+	else:
+		#print("REGULAR GRAVITY")
+		acceleration.y = REGULAR_GRAVITY
 	
-	#See if the character needs to drag to a halt
+	#Apply drag
 	if acceleration.x == 0:
 		#$CharAnims.play("idle")
+		#VELOCITY_TO_STOP_DRAGGING is the (very slow) speed where, if the player is moving below that speed, their velocity is set to zero.
+		#Used to prevent rapid switching of directions when they coast to a halt.
 		if(abs(velocity.x) > VELOCITY_TO_STOP_DRAGGING) :
 			if is_on_ground():
-				acceleration.x = DRAG_COEF * clamp(velocity.x, -1, 1)
+				acceleration.x = -GROUND_DRAG_DECELERATION * clamp(velocity.x, -1, 1)
 			else:
-				acceleration.x = AIR_DRAG * clamp(velocity.x, -1, 1)
+				acceleration.x = -AIR_DRAG_DECELERATION * clamp(velocity.x, -1, 1)
 		else:
 			velocity.x = 0
 #	else:
@@ -70,10 +80,11 @@ func _physics_process(delta):
 #			$Sprite.flip_h=false
 #		$CharAnims.play("walk")
 	
-	# Apply acceleration modifier & do other stuff
+	# Apply acceleration
 	if acceleration.length() > 0:
-		acceleration *= ACCELERATION_MODIFIER
 		velocity += acceleration * delta
+		#Clamp velocity to max
 		if(abs(velocity.x) > MAX_SPEED):
 			velocity.x = clamp(velocity.x, -1, 1) * MAX_SPEED
+	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
