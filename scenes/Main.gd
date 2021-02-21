00606287d2f8fd65fde7580ddf4251b592c78fb4
npb <apr0alt@gmail.com>
2021-02-21 06:17:16 -0500

    Merge conflicts should be finished

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -24,14 +24,35 @@ func _update_ui():
 	$UILayer/HeartbeatLabel.text = "Heartbeat: " + str(Globals.heartbeat)
 
 func _unhandled_input(event):
-	if event.is_action_pressed("bullettime") && bullettime == false && Globals.heartbeat > 70:
+	if event.is_action_pressed("bullettime") && bullettime == false && Globals.heartbeat > 70 && Globals.gameover == false:
 		$CanvasModulate.set_color(Color(0.8,0.8,1))
 		Engine.time_scale = Globals.SLOW_TIME_SCALE
 		$BulletTimeDuration.start()
 		Globals.heartbeat = 30
+		
+	if(Globals.gameover == true && event.is_action_pressed("restart")):
+		_restart_game()
 
 
 func _on_BulletTimeDuration_timeout():
-	print("")
 	Engine.time_scale = 1
 	$CanvasModulate.set_color(Color(1,1,1))
+	
+func game_over():
+	$UILayer/HPLabel.visible = false
+	Globals.gameover = true
+	$UILayer/RetryLabel.visible = true
+	#print("GAME OVER")
+
+func _restart_game():
+	
+	$Player.health = $Player.MAX_HEALTH
+	$Player.position = $PlayerSpawn.position
+	Globals.gameover = false
+	$UILayer/RetryLabel.visible = false
+	$UILayer/HPLabel.visible = true
+	Globals.heartbeat = 0
+	_on_BulletTimeDuration_timeout()
+	get_tree().call_group("Enemies", "respawn")
+	get_tree().call_group("Bullets", "queue_free")
+	#reset heartbeat, reset health of player and enemies, despawn bullets, set location of player, set enemies to alive
--- scenes/characters/BlaiseEnemy.gd
+++ scenes/characters/BlaiseEnemy.gd
@@ -6,15 +6,19 @@ const ESTIMATED_BULLET_TIME : float = 0.18 # estimated time for bullet to hit pl
 
 const MAX_HEALTH = 20
 var health = MAX_HEALTH
-var current_rotation_speed = 0
 var alive = true
+var player_in_sight = true
+
 #The export vars are used to differentiate the different enemies
 export var baserotation = 0
 
 func _ready():
 	for child in get_children():
 		if(child.is_in_group("BaseRotates")):
+			child.position = child.position.rotated(rotation_degrees*PI/180)
 			child.rotation_degrees = baserotation
+
+
 
 func die():
 	alive = false
@@ -30,7 +34,7 @@ func hurt(damage):
 
 
 # Find the position to target
-func find_target(player_pos, player_vel):
+func find_target(player_pos):
 	var target_position = player_pos + Globals.ADJUSTMENT_TO_CENTER_OF_PLAYER
 	return atan2((target_position.y - global_position.y), (target_position.x - global_position.x))
 
@@ -40,20 +44,38 @@ func _physics_process(delta):
 	#$RotatingPart.rotate(find_target(get_parent().get_parent().get_node("Player").global_position, get_parent().get_parent().get_node("Player").velocity))
 	
 	if(alive):
-		$Barrel.global_rotation = find_target(get_parent().get_parent().get_node("Player").global_position, get_parent().get_parent().get_node("Player").velocity)
-
+		$PlayerDetector.position = Vector2($PlayerDetector.position.length(),0).rotated(find_target(get_parent().get_parent().get_node("Player").global_position))
+		$PlayerDetector.global_rotation = find_target(get_parent().get_parent().get_node("Player").global_position)
+		
+		if($PlayerDetector.get_collider() == get_parent().get_parent().get_node("Player/Hurtbox")):
+			if(player_in_sight == false):
+				#The player just came into view!
+				$ShootTimer.start()
+				player_in_sight = true
+				$anims.play("hostile")
+			#The player has been in view for a while!
+			$BulletSpawn.position = Vector2($BulletSpawn.position.length(),0).rotated(find_target(get_parent().get_parent().get_node("Player").global_position))
+			$Barrel.global_rotation = find_target(get_parent().get_parent().get_node("Player").global_position)
+		else:
+			if(player_in_sight == true):
+				#The player just went out of view!
+				$anims.play("idle")
+				$Barrel.global_rotation = baserotation*PI/180
+				player_in_sight = false
+				$ShootTimer.stop()
+			
+		
 
 func _on_ShootTimer_timeout():
 	$anims.play("shoot")
 
 func shootbullet():
 	var bullet = BULLET.instance()
-	bullet.global_position = global_position
-	bullet.global_rotation = find_target(get_parent().get_parent().get_node("Player").global_position, get_parent().get_parent().get_node("Player").velocity)
+	print($BulletSpawn.global_position)
+	bullet.global_position = $BulletSpawn.global_position
+	bullet.global_rotation = find_target(get_parent().get_parent().get_node("Player").global_position)
 	bullet.target_player()
 	get_tree().get_root().add_child(bullet)
-	print("This z: ", str(z_index))
-	print("bullet z: ", str(bullet.z_index))
 	
 
 
@@ -61,3 +83,10 @@ func _on_Hurtbox_area_entered(bullet):
 	if(alive):
 		hurt(bullet.DAMAGE)
 	bullet.queue_free()
+
+
+func respawn():
+	health = MAX_HEALTH
+	alive = true
+	$anims.play("idle")
+	
